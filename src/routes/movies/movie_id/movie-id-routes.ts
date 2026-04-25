3c331dbefbe060b48a1c558b6d79f971fe643b39
Maria Ciorâțanu <mariacioratanu49@gmail.com>
2026-04-25 20:37:02 +0300

    ensure movie view events bypass detail cache

--- src/routes/movies/movie_id/movie-id-routes.ts
+++ src/routes/movies/movie_id/movie-id-routes.ts
@@ -25,7 +25,7 @@ const routes: RouteOptions[] = [
   {
     method: [HttpMethods.GET, HttpMethods.HEAD],
     url: endpoint,
-    schema: { ...FetchMovieSchema, tags: [...tags, RouteTags.CACHE] },
+    schema: { ...FetchMovieSchema, tags },
     handler: async function fetchMovie(request, reply) {
       const params = request.params as MovieIdObjectSchemaType;
       const movie = (await this.dataStore.fetchMovie(params.movie_id)) as MovieSchemaType;
